9925afe13cf1e36b3aa920de685dd5b2fe4efc6d
ultraviolet <75735238+uultraviolettt@users.noreply.github.com>
2021-01-16 21:04:37 +0200

    It's all

--- lesson2/hw3.py
+++ lesson2/hw3.py
@@ -2,15 +2,15 @@ print("Hello! You can calculate smth here, but be attentive ^-^")
 print("What would u like to calculate?")
 print("At first, enter one number, that enter one operator and one more number and one more operator.../n")
 
-oper = None
 res = 0
+oper = None
 
 while oper != "=":
     try:
         num = float(input("Please, enter a number: "))
     except ValueError:
         print("I am a joke to you???)))")
-        continue
+
     if oper is None:
         res = num
     elif oper == "+":
@@ -23,18 +23,17 @@ while oper != "=":
         try:
             res /= num
         except ZeroDivisionError:
-            print("Okey, I know, miracles happen, but not zero division. So, try again ")
+            print("Okay, I know, miracles happen, but not zero division. So, try again ")
+            continue
 
-    while oper in ("+" or "-" or "/" or "+" or "="):
-        oper = input()
+    oper = input("Please, enter an operator: ")
+    while oper not in ("+", "-", "/", "+", "="):
+        print("What do u mean? I expected +, -, *, / or = ")
+        oper = input("Please, enter an operator: ")
         oper1 = oper.split(' ')
         if len(oper1) == 1:
             oper = oper
-        else:
             continue
-        if oper not in ("+" or "-" or "/" or "+" or "="):
-            print("What do u mean? I accepted +, -, * or /")
-            continue
-        elif "=":
-            continue
-    print(f"result = {res}")
+    if oper == "=":
+        continue
+print(f"result = {res}")
